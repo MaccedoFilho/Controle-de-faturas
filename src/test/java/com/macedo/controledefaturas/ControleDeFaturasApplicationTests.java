3151cd497652f330a3290405f50ffa33994ec395
MacedoFilho <ricardomacedor@icloud.com>
2025-04-05 00:19:35 -0300

    First commit - Estrutura montada

---
+++ src/test/java/com/macedo/controledefaturas/ControleDeFaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macedo.controledefaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeFaturasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
