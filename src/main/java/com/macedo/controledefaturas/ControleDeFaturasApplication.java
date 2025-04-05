b45c464a797b05bb2be04586517bc4df2c11627f
Ricardo Roberto Macedo Filho <ricardomacedodev@gmail.com>
2025-04-05 00:19:35 -0300

    First commit - Estrutura montada

---
+++ src/main/java/com/macedo/controledefaturas/ControleDeFaturasApplication.java
@@ -0,0 +1,13 @@
+package com.macedo.controledefaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeFaturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleDeFaturasApplication.class, args);
+    }
+
+}
