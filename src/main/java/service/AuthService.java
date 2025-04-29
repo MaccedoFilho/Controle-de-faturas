aef0f49110100095a88bc876da868a083ea435a9
MacedoFilho <ricardomacedor@icloud.com>
2025-04-29 02:04:29 -0300

    feat: implementa login básico com validação de email e senha

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -2,10 +2,28 @@ package service;
 
 import dto.LoginRequest;
 import dto.LoginResponse;
+import model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import repository.UserRepository;
 
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     public LoginResponse login(LoginRequest loginRequest) {
-        LoginResponse loginResponse = new LoginResponse();
-        return loginResponse;
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("password does not match");
+        }
+
+        return new LoginResponse();
     }
 }
